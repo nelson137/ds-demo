4bc3952d39ddfc63f8ad2cad1d6026f96d02d7b0
Nelson Earle <nelson.earle137@gmail.com>
2017-09-01 21:02:03 +0000

    updated venv for rename

--- ds-demo/venv/bin/activate.fish
+++ ds-demo/venv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/var/www/ds-form-site/ds-form-site/venv"
+set -gx VIRTUAL_ENV "/var/www/ds-demo/ds-demo/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
