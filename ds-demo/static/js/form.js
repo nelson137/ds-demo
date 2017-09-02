01c03cc2d54565e63815e2b46776f993c1db4a3b
Nelson Earle <nelson.earle137@gmail.com>
2017-09-02 12:47:44 +0000

    moved auto-hide flash to template, started validate implementation

--- ds-demo/static/js/form.js
+++ ds-demo/static/js/form.js
@@ -1,16 +1,38 @@
-setTimeout(function() {
-  document.getElementById("flash").style.display = "none";
-}, 3000);
-
-function nameOnKeyDown() {
-  text = document.getElementById("");
+function validateName(event) {
+  if (event.key != 'a') {
+    event.preventDefault();
+  }
 }
 
-function priceOnKeyDown() {
-  text = document.getElementById("");
+function validatePrice(event) {
+  event.preventDefault();
+  elem = event.srcElement;
+
+  function insert(substr) {
+    elem.value = elem.value.slice(0, elem.selectionStart) + substr + elem.value.slice(elem.selectionEnd);
+  }
+
+  text = event.key || event.clipboardData.getData('Text');
+  for (i=0; i<text.length; i++) {
+    if ('0123456789'.includes(text[i])) {
+      insert(text[i]);
+    } else if (text[i] == '$' && elem.selectionStart == 0 && (elem.value.match(/\$/g) || []).length < 1) {
+      elem.value = '$' + elem.value;
+      elem.selectionStart = 1;
+      elem.selectionEnd = 1;
+    } else if (text[i] == ' ' && (elem.value.match(/\./g) || []).length < 1) {
+      if (text[i] == '.' ||text[i] == ' ')
+      insert('.');
+    } else if ((elem.value.match(/\./g) || []).length < 1) {
+      insert('.');
+    } else if ('0123456789'.includes(text[i])) {
+      insert(text[i]);
+    }
+  }
 }
 
-function reviewsOnKeyDown() {
-  text = document.getElementById("");
+function validateRatings(event) {
+  console.log(event);
+  text = document.getElementById('ratings');
 }
 
