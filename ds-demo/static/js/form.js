75f18badf6ff3be2182ec4555268803e769c8ed7
Nelson Earle <nelson.earle137@gmail.com>
2017-09-02 06:21:35 +0000

    added remove flash func and onKeyDown funcs

---
+++ ds-demo/static/js/form.js
@@ -0,0 +1,16 @@
+setTimeout(function() {
+  document.getElementById("flash").style.display = "none";
+}, 3000);
+
+function nameOnKeyDown() {
+  text = document.getElementById("");
+}
+
+function priceOnKeyDown() {
+  text = document.getElementById("");
+}
+
+function reviewsOnKeyDown() {
+  text = document.getElementById("");
+}
+
