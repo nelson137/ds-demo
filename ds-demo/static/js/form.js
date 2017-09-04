0612df17c0442e500514536d2e6befa6e64e49ed
Nelson Earle <nelson.earle137@gmail.com>
2017-09-04 09:11:06 +0000

    finished input validations

--- ds-demo/static/js/form.js
+++ ds-demo/static/js/form.js
@@ -1,38 +1,54 @@
-function validateName(event) {
-  if (event.key != 'a') {
-    event.preventDefault();
-  }
-}
+var invalidPrice = false;
+var invalidRatings = false;
 
 function validatePrice(event) {
-  event.preventDefault();
-  elem = event.srcElement;
-
-  function insert(substr) {
-    elem.value = elem.value.slice(0, elem.selectionStart) + substr + elem.value.slice(elem.selectionEnd);
+  text = event.srcElement.value;
+  invalid = false;
+  for (i=0; i<text.length; i++) {
+    if (! '0123456789.'.includes(text[i])) {
+      invalid = true;
+    }
+  }
+  if ((text.match(/\./g) || []).length > 1) {
+      invalid = true;
   }
 
-  text = event.key || event.clipboardData.getData('Text');
-  for (i=0; i<text.length; i++) {
-    if ('0123456789'.includes(text[i])) {
-      insert(text[i]);
-    } else if (text[i] == '$' && elem.selectionStart == 0 && (elem.value.match(/\$/g) || []).length < 1) {
-      elem.value = '$' + elem.value;
-      elem.selectionStart = 1;
-      elem.selectionEnd = 1;
-    } else if (text[i] == ' ' && (elem.value.match(/\./g) || []).length < 1) {
-      if (text[i] == '.' ||text[i] == ' ')
-      insert('.');
-    } else if ((elem.value.match(/\./g) || []).length < 1) {
-      insert('.');
-    } else if ('0123456789'.includes(text[i])) {
-      insert(text[i]);
-    }
+  icon = document.getElementById('warn-icon-price');
+  if (invalid) {
+    icon.style.display = 'block';
+    invalidPrice = true;
+  } else {
+    icon.style.display = 'none';
+    invalidPrice = false;
   }
 }
 
 function validateRatings(event) {
-  console.log(event);
-  text = document.getElementById('ratings');
+  text = event.srcElement.value;
+  invalid = false;
+  for (i=0; i<text.length; i++) {
+    if (! '0123456789'.includes(text[i])) {
+      invalid = true;
+    }
+  }
+
+  icon = document.getElementById('warn-icon-ratings');
+  if (invalid) {
+    icon.style.display = 'block';
+    invalidRatings = true;
+  } else {
+    icon.style.display = 'none';
+    invalidRatings = false;
+  }
+}
+
+function onSubmit() {
+  if (! invalidPrice && ! invalidRatings) {
+    return true;
+  } else {
+    if (invalidPrice) { console.log('Price field is invalid'); }
+    if (invalidRatings) { console.log('Ratings field is invalid'); }
+    return false;
+  }
 }
 
